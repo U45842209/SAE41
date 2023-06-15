28a7654620cb207cc3294d126e03f22494b797dc
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-06-15 08:32:42 +0000

    Initial commit

--- ips.sh
+++ ips.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Récupérer tous les ID de conteneurs Docker en cours d'exécution
-container_ids=$(docker ps -q)
+# Récupérer tous les ID et noms de conteneurs Docker en cours d'exécution
+containers=$(docker ps --format "{{.ID}} {{.Names}}")
 
-# Parcourir tous les conteneurs et afficher leurs adresses IP
-for container_id in $container_ids
+# Parcourir tous les conteneurs et afficher leurs adresses IP avec les noms
+while read -r container_id container_name
 do
     container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
-    echo "Conteneur $container_id : $container_ip"
-done
+    echo "Conteneur $container_name : $container_ip"
+done <<< "$containers"
+
