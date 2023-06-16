e8a4fda663d7328bf009245cf81d2167ac065bfe
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-06-16 09:58:16 +0000

    *

--- app/main.py
+++ app/main.py
@@ -37,24 +37,32 @@ app = FastAPI(middleware=middlewares)
 #Connexion à la BDD
 try:
     mydb = mysql.connector.connect(
-
-        host="localhost",
-        user="user_admin",
-        password="Password1234*",
-        database="SAE410"
+        host="172.20.0.10",
+        user="root",
+        password="password",
+        database="mysql"
     )
     sql_cursor = mydb.cursor()
+
+    # Vérifier si la base de données SAE410 existe
+    sql_cursor.execute("SHOW DATABASES LIKE 'SAE410'")
+    result = sql_cursor.fetchone()
+
+    if result is None:
+        # Créer la base de données SAE410
+        sql_cursor.execute("CREATE DATABASE SAE410")
+        print("La base de données SAE410 a été créée")
+
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
     else:
         print(err)
 
-else:
-    mydb.close()
-
+finally:
+    if mydb.is_connected():
+        sql_cursor.close()
+        mydb.close()
 
 
 
