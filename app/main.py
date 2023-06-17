29de577797ccf3a8db238d85b9af755d95163340
Pierre H <46249979+Darkenston@users.noreply.github.com>
2023-06-17 17:09:48 +0200

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, Form, Response, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse, PlainTextResponse
-from fastapi.middleware import Middleware
+from fastapi.routing import APIRoute
 from typing import Annotated
 import mysql.connector
 from mysql.connector import errorcode
@@ -10,60 +10,37 @@ import webbrowser
 
 
 #Partie qui permet de bloquer des pages si le cookie n'est pas set avec le principe du middleware
-class CookieAuthMiddleware:
-    def __init__(self, app):
-        self.app = app
 
-    async def __call__(self, scope, receive, send):
-        request = Request(scope, receive)
-        
-        # Vérifie l'url
-        if request.url.path == "/rdv":
-            # Vérifie la présence du cookie
-            if "username" not in request.cookies:
-                return PlainTextResponse("Accès interdit. Veuillez vous connecter.", status_code=403)
 
-        # Le cookie est présent, poursuivre avec la requête normale
-        response = await self.app(scope, receive, send)
-        return response
 
 #Ajout du middleware à FastAPI
-middlewares = [
-    Middleware(CookieAuthMiddleware),
-]
-app = FastAPI(middleware=middlewares)
+app = FastAPI()
 
 
 #Connexion à la BDD
 try:
     mydb = mysql.connector.connect(
-        host="172.20.0.10",
-        user="root",
-        password="password2",
+
+        host="localhost",
+        user="user_admin",
+        password="Password1234*",
         database="SAE410"
     )
     sql_cursor = mydb.cursor()
-
-    # Vérifier si la base de données SAE410 existe
-    sql_cursor.execute("SHOW DATABASES LIKE 'SAE410'")
-    result = sql_cursor.fetchone()
-
-    if result is None:
-        # Créer la base de données SAE410
-        sql_cursor.execute("CREATE DATABASE SAE410")
-        print("La base de données SAE410 a été créée")
-
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
     else:
         print(err)
 
-finally:
-    if mydb.is_connected():
-        sql_cursor.close()
-        mydb.close()
 
+
+
+
+
+print("Ok")
 
 #Initialisation des templates pour jinja
 templates = Jinja2Templates(directory="templates")
@@ -120,11 +97,37 @@ def get_username(request: Request):
     return request.cookies.get("username")
 
 
+
 @app.get("/rdv")
-async def rdv(request: Request):
+async def rdv(request: Request, usernamelog: str = Depends(get_username)):
     username = request.cookies.get("username")
     print(username)
-    return templates.TemplateResponse("page_user.html", {"request": request, "username": username})
+
+    # Partie tableau de RDV
+    table_rdv = {}
+    #On récupère l'id utilisateur
+    sql = "SELECT id FROM users WHERE username = %s"
+    valsql = (usernamelog,)
+    sql_cursor.execute(sql, valsql)
+    resultsql1 = sql_cursor.fetchone()
+
+    #On récupère les rdv de l'utilisateur
+    sqlTable = "SELECT * FROM rdv WHERE user_id = %s"
+    valtable = (resultsql1[0],)
+    sql_cursor.execute(sqlTable, valtable)
+    resultsqlTable = sql_cursor.fetchall()
+    print(resultsqlTable)
+
+    for line in resultsqlTable:
+        item_id = str(line[0])
+        item = {
+            "objet": line[2],
+            "date": line[3].strftime("%Y-%m-%d")
+        }
+        print(line)
+        table_rdv[item_id] = item
+
+    return templates.TemplateResponse("page_user.html", {"request": request, "username": username, "table_rdv": table_rdv})
 
 
 
@@ -145,14 +148,9 @@ async def rdvpost(objet: Annotated[str, Form()], date: Annotated[str, Form()], u
     sql_cursor.execute(sql2, val)
     mydb.commit()
 
-    #Partie tableau de RDV
-    """
-    sqlTable = "SELECT * FROM rdv WHERE user_id = %s"
-    valtable = (resultsql1[0],)
-    sql_cursor.execute(sqlTable, valtable)
-    resultsqlTable = sql_cursor.fetchall()
-    print(resultsqlTable)"""
 
+
+    return RedirectResponse(url="/rdv", status_code=303)
 
 
 @app.post("/logout")
